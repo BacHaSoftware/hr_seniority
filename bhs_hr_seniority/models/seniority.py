f71927a6bdd19b8b137ccf99ed3113f81f718a21
“YenTran” <yentran.bhtech@gmail.com>
2026-07-30 16:07:21 +0700

    v19: update code

--- bhs_hr_seniority/models/seniority.py
+++ bhs_hr_seniority/models/seniority.py
@@ -1,19 +1,24 @@
+import datetime
+from datetime import datetime, timedelta
+from _datetime import date, datetime
 from odoo import models, fields, api, _
 
 
 class Bhseniority(models.Model):
     _inherit = 'hr.employee'
 
-    seniority = fields.Char(string='Seniority', compute='_compute_seniority', compute_sudo=True)
+    seniority = fields.Char(string='Seniority', compute='_compute_seniority')
+    joining_first_date = fields.Date(
+        string='First Date',
+        compute='_compute_joining_first_date',
+        store=True,
+    )
 
-    @api.depends('version_ids.date_start', 'version_ids.date_end')
     def _compute_seniority(self):
-        # self.joined_date = self.employee_id.joining_date if self.employee_id.joining_date else ''
         for rec in self:
-            today = fields.Date.today()
-            first_contract_date = rec.sudo()._get_first_version_date()
-            if first_contract_date:
-                days = (today - first_contract_date).days
+            today = date.today()
+            if rec.joining_first_date:
+                days = (today - rec.joining_first_date).days
                 if days < 365:
                     month = int(days) // 30
                     rec.seniority = _('%s months', month)
@@ -23,3 +28,14 @@ class Bhseniority(models.Model):
                     rec.seniority = _('%s years %s months', year, month)
             else:
                 rec.seniority = 'Not define'
+
+    @api.depends('version_ids.contract_date_start')
+    def _compute_joining_first_date(self):
+        for employee in self:
+            versions = employee.sudo().version_ids.filtered(
+                lambda v: v.contract_date_start
+            ).sorted(key=lambda v: v.contract_date_start)
+
+            employee.joining_first_date = (
+                versions[0].contract_date_start if versions else False
+            )
